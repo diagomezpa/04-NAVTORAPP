2b710d80b60d025683bdf6f3380ba0e9e5d7835c
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-10 16:20:00 -0500

    seccion 7 video 110. Toggle Drawer - Mostar / Ocultar

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,28 @@
 /* eslint-disable prettier/prettier */
-import {type NavigationProp, useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {View} from 'react-native';
+import {
+  type NavigationProp,
+  useNavigation,
+  DrawerActions,
+} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Pressable, Text, View} from 'react-native';
 import {globalStyles} from '../../theme/theme';
 import {PrimaryButton} from '../../components/shared/PrimaryButton';
 import type {RootStackParams} from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menu</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
 
   return (
     <View style={globalStyles.container}>
