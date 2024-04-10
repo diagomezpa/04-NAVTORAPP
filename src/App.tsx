b51d9cfd6370e5a863643c4de7c74724575965d2
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-09 22:42:42 -0500

    seccion video Detener

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable prettier/prettier */
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StackNavigator} from './presentation/routes/StackNavigator';
+import {SideMenuNavigator} from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <SideMenuNavigator />
     </NavigationContainer>
   );
 };
---
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import {Text, View} from 'react-native';
+
+export const ProfileScreen = () => {
+  return (
+    <View>
+      <Text> hola</Text>
+    </View>
+  );
+};
